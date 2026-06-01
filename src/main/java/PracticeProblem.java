5289bd4f758d0e7e07832f141cbd8f4202ea1f07
SalmaDanane <343249595@tdsb.ca>
2026-06-01 10:38:13 -0400

    Update PracticeProblem.java
    Done!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,40 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static String isFive(int num) {
+		if (num == 5) {
+			return "The number is Five";
+		}
+		else {
+			return "The number is not Five";
+		}
+	}
 
+	public static String positiveOrNegative(double num) {
+		if (num > 0) {
+			return "Positive";
+		}
+		else {
+			return "Negative";
+		}
+	}
+
+	public static String highOrLow(int num) {
+		if (num > 100) {
+			return "High";
+		}
+		else {
+			return "Low";
+		}
+	}
+
+	public static String isHello(String word) {
+		if (word == "Hello") {
+			return "The word is Hello";
+		}
+		else {
+			return "The word is not Hello";
+		}
+	}
 }
+
